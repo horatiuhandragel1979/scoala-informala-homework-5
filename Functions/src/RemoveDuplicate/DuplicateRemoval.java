edb273a59b34cae6e35f207f13a9130f9fd7ce2a
horatiuhandragel1979 <horatiu1979@gmail.com>
2020-01-07 14:11:45 +0200

    commit functions homework

--- Functions/src/RemoveDuplicate/DuplicateRemoval.java
+++ Functions/src/RemoveDuplicate/DuplicateRemoval.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
 
 /**
  * the programm removes all the duplicates found in an int[] array
-*
-*@author Horatiu Handragel
+ *
+ * @author Horatiu Handragel
  */
 
 public class DuplicateRemoval {
     public static void main(String[] args) {
+        inputArray();
 
-        /**
-         * input the string of integers from the keyboard
-         */
+    }
 
+
+    /**
+     * the method input from keyboard the array numbers
+     */
+
+    public static void inputArray() {
 
         System.out.println("Introdu lungimea sirului: ");
         Scanner sc = new Scanner(System.in);
@@ -31,9 +36,10 @@ public class DuplicateRemoval {
          * the Arrays.sort() puts all elements of a matrix in ascending order
          */
         Arrays.sort(arr);
-        System.out.println("Acesta este sirul dupa eliminarea dublurilor:");
         int length = arr.length;
         length = removeDuplicateElements(arr, length);
+        System.out.println("Acesta este sirul dupa eliminarea dublurilor:");
+
 
         /**
          * we display the elements of the string, without duplicates
@@ -41,23 +47,28 @@ public class DuplicateRemoval {
 
         for (int i = 0; i < length; i++)
             System.out.println(arr[i] + "  ");
+        return;
+
     }
 
     /**
-     *the method identifies duplicates in the string
+     * the method remove duplicates from array list
+     *
+     * @param arr
+     * @param n
+     * @return
      */
-
-    public static int removeDuplicateElements(int arr[], int n){
-        if (n==0 || n==1){
+    public static int removeDuplicateElements(int arr[], int n) {
+        if (n == 0 || n == 1) {
             return n;
         }
         int j = 0;//for next element
-        for (int i=0; i < n-1; i++){
-            if (arr[i] != arr[i+1]){
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
                 arr[j++] = arr[i];
             }
         }
-        arr[j++] = arr[n-1];
+        arr[j++] = arr[n - 1];
         return j;
     }
 }
